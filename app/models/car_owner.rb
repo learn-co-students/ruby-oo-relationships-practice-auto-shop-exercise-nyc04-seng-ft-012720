16c69d7ce8fb540cb93079b3fb1eb19260ecfe99
mav-code <mavisansmembres@gmail.com>
2020-02-04 13:06:09 -0500

    Done.

--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -1,10 +1,26 @@
 class CarOwner
 
   attr_reader :name
-
+  @@all = []
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def cars
+    Car.all.select{|car| car.owner == self}
+  end
+
+  def mechanics
+    self.cars.map{|car| car.mechanic}
+  end
+
+  def self.average_number_of_cars
+    Car.all.length.to_f / self.all.length.to_f
+  end
 
 end
